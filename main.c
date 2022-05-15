a858c90a3a9a8373a35fd7d5f373064856828258
antoine <antoineabonneau94@gmail.com>
2022-05-15 08:32:31 -0400

    Fixed connection to server

--- main.c
+++ main.c
@@ -1,29 +1,64 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h> 
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 
-#define FILENAME "messageTemp.data"
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 6969
+#define MESSAGE_LENGTH 256
+
+typedef int SOCKET;
+
+void initConnectionToServer (SOCKET *sock, struct sockaddr_in server);
+void sendUserInputToServer (SOCKET sock, struct sockaddr_in server, int maxDataLength);
+void handleError(char *message);
 
 int main () {
-    FILE *output = fopen(FILENAME, "w");
+    SOCKET sock;
+    struct sockaddr_in server;
 
-    time_t rawTime;
-    struct tm *timeInfo;
+    initConnectionToServer(&sock, server);
 
-    time(&rawTime);
+    sendUserInputToServer(sock, server, MESSAGE_LENGTH);
 
-    timeInfo = localtime(&rawTime);
-
-    int hours = timeInfo->tm_hour;
-    int minutes = timeInfo->tm_min;
-    int seconds = timeInfo->tm_sec;
-
-    if (output == NULL) {
-        printf("Error opening file.");
-        exit(EXIT_FAILURE);
-    }
-
-    fprintf(output, "[%d:%d:%d] Hello World!", hours, minutes, seconds);
+    close(sock);
 
     return 0;
 }
+
+
+void initConnectionToServer (SOCKET *sock, struct sockaddr_in server) {
+    *sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket < 0) handleError("Error creating socket");
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr(SERVER_IP);
+    server.sin_port = htons(SERVER_PORT);
+
+    int connection = connect(*sock, (struct sockaddr *)&server, sizeof(server));
+    if (connection < 0) handleError("Failed to connect to remote host");
+}
+
+
+void sendUserInputToServer (SOCKET sock, struct sockaddr_in server, int maxDataLength) {
+    char data[maxDataLength];
+
+    printf("> "); fgets(data, maxDataLength, stdin);
+
+    int dataLength = strlen(data) + 1;
+
+    sendto(sock, data, dataLength, 0, (struct sockaddr*)&server, sizeof(server));
+}
+
+
+void handleError(char *message) {
+    printf("%s", message);
+    exit(EXIT_FAILURE);
+}
+
+
