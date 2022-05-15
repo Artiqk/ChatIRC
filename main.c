ef1bd34ba4483a5646887750b29cba64446ab3fc
antoine <antoineabonneau94@gmail.com>
2022-05-15 16:10:53 -0400

    Moved custom functions to header file

--- main.c
+++ main.c
@@ -1,73 +1,23 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h> 
-#include <netdb.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define SERVER_IP "127.0.0.1"
-#define SERVER_PORT 6969
-#define MESSAGE_LENGTH 256
-
-typedef int SOCKET;
-
-void initConnectionToServer (SOCKET *sock, struct sockaddr_in server);
-int sendUserInputToServer (SOCKET sock, struct sockaddr_in server, int maxDataLength);
-void handleError(char *message);
+#include "chat_irc.h"
 
 int main () {
     SOCKET sock;
     struct sockaddr_in server;
+    pthread_t dataRecv;
+    pthread_t sendData;
     int connected = 1;
 
     initConnectionToServer(&sock, server);
 
     while (connected) {
+        pthread_create(&dataRecv, NULL, getRemoteServerData, (void*)&sock);
+
         connected = sendUserInputToServer(sock, server, MESSAGE_LENGTH);
+
+        pthread_join(dataRecv, NULL);
     }
 
     close(sock);
 
     return 0;
-}
-
-
-void initConnectionToServer (SOCKET *sock, struct sockaddr_in server) {
-    *sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (socket < 0) handleError("Error creating socket");
-
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr(SERVER_IP);
-    server.sin_port = htons(SERVER_PORT);
-
-    int connection = connect(*sock, (struct sockaddr *)&server, sizeof(server));
-    if (connection < 0) handleError("Failed to connect to remote host");
-}
-
-
-int sendUserInputToServer (SOCKET sock, struct sockaddr_in server, int maxDataLength) {
-    char data[maxDataLength];
-
-    printf("> "); fgets(data, maxDataLength, stdin);
-
-    data[strcspn(data, "\n")] = '\0';
-
-    if (strcmp(data, "disconnect") == 0) {
-        return 0;
-    }
-
-    int dataLength = strlen(data) + 1;
-
-    sendto(sock, data, dataLength, 0, (struct sockaddr*)&server, sizeof(server));
-
-    return 1;
-}
-
-
-void handleError(char *message) {
-    printf("%s", message);
-    exit(EXIT_FAILURE);
 }
